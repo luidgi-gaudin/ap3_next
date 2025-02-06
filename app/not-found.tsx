5cddd4980e9bf97be776febba78746e5eb4794ab
luidgi <146814537+luidgi-gaudin@users.noreply.github.com>
2025-02-06 16:43:03 +0100

    correction apostrophe en &apo;

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -10,10 +10,10 @@ export default function NotFound() {
                 <CardContent className="flex flex-col items-center space-y-4">
                     <h1 className="text-4xl font-bold">404</h1>
                     <p className="text-center text-sm text-muted-foreground">
-                        Oups… la page que vous recherchez n'existe pas.
+                        Oups… la page que vous recherchez n&apos;existe pas.
                     </p>
                     <Link href="/dashboard">
-                        <Button>Retour à l’accueil</Button>
+                        <Button>Retour à l&apos;accueil</Button>
                     </Link>
                 </CardContent>
             </Card>
