7735c1a0fffd3fb0dc8734c69631d8f7c5e079ef
luidgi <146814537+luidgi-gaudin@users.noreply.github.com>
2025-02-12 11:21:55 +0100

    redirect automatique de / a la bonne page selon le role

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,33 @@
 import { redirect } from 'next/navigation';
+import { createClient } from '@/utils/supabase/server';
+import UtilisateurService from '@/services/utilisateurService';
 
-export default function Home() {
-    redirect('/dashboard');
+export default async function Home() {
+    const supabase = await createClient();
+    const { data: session, error } = await supabase.auth.getUser();
 
-    return <div>Redirection...</div>;
+    if (error || !session) {
+        // Si l'utilisateur n'est pas authentifié, redirigez vers /login
+        redirect('/login');
+    }
+
+    const user = await UtilisateurService.getUtilisateurBySupabaseId(session.user.id);
+
+    if (!user) {
+        // Si l'utilisateur n'est pas trouvé, redirigez vers /login
+        redirect('/login');
+    }
+
+    // Redirigez en fonction du rôle de l'utilisateur
+    switch (user.id_role) {
+        case 1:
+            redirect('/admin');
+            break;
+        case 2:
+            redirect('/dashboard');
+            break;
+        default:
+            redirect('/login');
+    }
+
 }
